5ef77398f49496c579582fa0bf16dfe485dee14b
Ike-Martha <amagwen36@gmail.com>
2024-04-08 14:46:05 +0100

    Add project files.

---
+++ ComplexIf/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.Design;
+namespace ComplexIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number between 1 and 100");
+            string userValue = Console.ReadLine();  
+            int compareValue = int.Parse(userValue);
+
+
+            if ((compareValue < 1) || (compareValue > 100))
+
+                Console.WriteLine("The number is out of bound");
+
+            else if
+            ((compareValue == 42) || (compareValue < 90))
+                Console.WriteLine("You found one of the special numbers!");
+            else
+                Console.WriteLine("You did not find one of the special numnbers");
+            Console.ReadLine();
+                
+           
+
+            
+            
+        }
+    }
+}
